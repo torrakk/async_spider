b536599480f83240e474f4a4b12a2a4bfd3b625a
paul carrot <carrotpaul@protonmail.com>
2019-04-01 16:15:54 +0200

    add the shebang in install_gecko

--- install_gecko.sh
+++ install_gecko.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz
 sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.21.0-linux64.tar.gz -O > /usr/bin/geckodriver'
 sudo chmod +x /usr/bin/geckodriver
