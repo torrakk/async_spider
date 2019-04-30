e1eb715ffe38fdb1510958d1a7c1b40d765d185c
paul carrot <carrotpaul@protonmail.com>
2019-04-30 15:34:37 +0200

    test enlevement du click 11

--- webcrawler/parser.py
+++ webcrawler/parser.py
@@ -88,11 +88,11 @@ class Parse():
         # try:
         if isinstance(element, bs4.element.ResultSet) or isinstance(element, list):
             obj = [ self.rechercheBF(motif, result) for result in element ]
-            parse_log.debug('BeautifulSoup !' + type(element) +  'typeRecherche : ' + typeRecherche) #+ " element bs :" + element)
+            parse_log.debug('BeautifulSoup ! 1 ' + type(element) +  'typeRecherche : ' + typeRecherche + " element bs :" + element)
             objetRetour = obj if not isinstance(obj[0], list) else list(chain.from_iterable(obj))#list(chain.from_iterable(obj))
         elif isinstance(element, bs4.BeautifulSoup) or isinstance(element, bs4.element.Tag):
             #print('NOUS SOMMES DANS UN ELEMENT BeautifulSoup !', type(element), element)
-            parse_log.debug('BeautifulSoup !' + str(type(element)) + 'typeRecherche : ' + typeRecherche) #+ " element bs :" + str(element))
+            parse_log.debug('BeautifulSoup ! 2 ' + str(type(element)) + 'typeRecherche : ' + typeRecherche + " element bs :" + str(element))
             #print("ok")
             if isinstance(valeursDeRecherche, dict):
                 objetRetour = self.__getBFmethod(element, typeRecherche)(**valeursDeRecherche)
