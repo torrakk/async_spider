a18a340f69fee1b6aaf5cfa2ccd3d1c8e10635d6
paul carrot <carrotpaul@protonmail.com>
2019-09-06 17:31:17 +0200

    Mise à jour de chaineDAction et Webelements

--- webcrawler/selenium_parse.py
+++ webcrawler/selenium_parse.py
@@ -16,8 +16,43 @@ from webcrawler.utils import xpath
 from webcrawler.settings import PAUSE
 
 def action(recherches):
+    '''
+    Générateurs sur les actions
+    :param recherches:
+    :return:
+    '''
+    rank = 0
+    length = len(recherches)
     for item in recherches:
+        rank += 1
         yield item
+
+def lastSearchAction(recherches):
+    '''
+    Fait remonté la dernière action de recherche, avec son type de retour
+    :param recherches:
+    :return:
+    '''
+
+def getItemType(item):
+    """
+    Cette fonction permet de retourner le type de retour de la fonction
+    :param item:
+    :return: list, single or None
+    """
+    try:
+        doc = item.__doc__.strip().replace("\n", "")
+    except(AttributeError):
+        doc = ''
+    DOCSTRING_LIST_WEBELEMENT = re.compile('^.*:Returns:.*- (list of WebElement).*$')
+    DOCSTRING_WEBELEMENT = re.compile('^.*:Returns:.*- (WebElement).*$')
+    if DOCSTRING_LIST_WEBELEMENT.match(doc):
+        return "list"
+    elif DOCSTRING_WEBELEMENT.match(doc):
+        return "single"
+    elif item in ('click', 'drag_and_drop', 'back'):
+        return
+    return
 
 def rechercheDsElement(element, methode):
     pass
@@ -33,6 +68,9 @@ class rechercheSelenium():
     def __init__(self, page, recherche):
         pass
 
+
+
+
 class chaineDAction():
     '''
     La chaîne d'action permet d'avoir un objet contenant tous les éléments d'une chaîne de recherche
@@ -44,13 +82,20 @@ class chaineDAction():
     '''
 
     ##Variables contenant tous les éléménts, plusieurs chaineDeRecherchent peuvent être inclues.
+    resultats = set([])
 
-    def __init__(self, recherches, webdriver):
-        self.recherches = action(recherches)
-        self.webdriver = webdriver
+    def __init__(self, recherches, page):
+        '''
+        :param recherches: chaines de recherches
+        :param page: Webdriver instancié
+        '''
+
+        # crée un generateur qui permet de prendre les actions les unes après les autres
+        self.action = action(recherches)
+        self.page = page
         self.chaine = set([])
 
-    def finRecherche(self):
+    def finAction(self):
         """
         Acte la fin de recherche pour une chaîne de recherche
         :return:
@@ -59,15 +104,113 @@ class chaineDAction():
             return True
         return False
 
-    def recherche(self):
-        while not self.finRecherche():
-            pass
+    def chaineAction(self):
+        '''
+        Permet d'excuter une suite d'actions. Ces dernières vont être menées jusqu'au bout, c'est à dire que nous executerons
+        le suite d'action sur un élément avec de passer à un autre.
+        :return:
+        '''
+        while not self.finAction():
+            webelems = webElements(next(self.action), self.page)
+            self.chaine = webelems.run(self)
+            
+            # TODO Mettre en place ici la recherche des webelements
+            # TODO il faut faire une fonction recursive qui rappel chaineAction afin que nous allions toujours
+
 
 
 from types import MethodType
 # https://www.ianlewis.org/en/dynamically-adding-method-classes-or-class-instanc
+class webElements():
+    '''
+    Serie d'éléments remontés d'une recherche, sur une fenêtre (windowAction)
+    L'objet webelement pilote l'objet windowAction qui se deplace sur la page.
+    1) Nous faisons l'action dans la page --> methode run
+        1-a) Si l'action conduit à une recherche d'autres éléments alors nous produisons
+             une chaine d'action --> chaineAction
+        1-b) Si l'action ne mêne à rien ou ne renvoie rien alors nous faisons un --> Return True
+    Les actions sont soit enregistrées dans un resultat dans webElements soit dans un resultat de chainAction si ce sont les derniers
+    '''
 
-# class composition(ABC):
+    PAUSE = 4
+
+    def __init__(self, recherche, page ):
+        self.page = page
+        self.url = self.page.getCurrentUrl()
+        self.action = recherche
+        self.window = windowAction(self.page)
+
+    def __getSeleniumMethod(self, resu, cle):
+        '''
+        Méthode permettant d'accéder aux méthodes de de selenium
+        :param resu:
+        :param cle:
+        :return:
+        '''
+        print(set(map(type, resu)), " : " , cle)
+        try:
+            if hasattr(resu, cle):
+                #print('l\'element {} possede la methode {}'.format(resu, cle))
+                return getattr(resu, cle)
+            else:
+                return getattr(self.page, cle)
+        except(Exception) as e:
+            print("La methode selenium ne semble pas exister {}".format(cle))
+            raise
+
+    def seleniumActionBase(self, item, action, args=None):
+
+        try:
+            methodeSelenium = self.__getSeleniumMethod(item, action)
+        except(Exception) as e:
+            print(e, traceback.format_exc())
+            print("L'objet {} n'a pas de méthode {}".format(item, action))
+            raise
+        return methodeSelenium(args) if args else methodeSelenium()
+
+    def run(self, item, action, args=None):
+
+        '''
+        Methode permettant de rechercher de faire une recherche dans la page
+        :param item: element selenium
+        :param action: Methode selenium a appliquer
+        :param args: Arguments accompagnant la methode selenium
+        :return:
+        '''
+
+        onContinue = True
+        inter = set([])
+
+        #La boucle ici permet de rechercher des éléments à l'intérieur de la fenêtre courante
+
+        while onContinue:
+            try:
+                trouve = self.seleniumActionBase(item, action, args)
+                typoItem = getItemType(item)
+                if typoItem == 'liste' and trouve:
+                    for item in trouve:
+                         inter.update(item)
+                elif typoItem == 'single' and trouve:
+                    inter = trouve
+            except(Exception) as e:
+                print(e, traceback.format_exc())
+                raise
+            ## Si nous sommes au dernier rang de recherche alors nous
+            ## continuons la recherche
+
+            ## TODO finir la problématique ici, soit il faut continuer le scroll, si nous sommes au dernier rang de recherche
+            ## TODO ajouter l'éléments père à la chaîne d'action pour que l'élément enfant puisse dire au père d'avancer
+            ## TODO modifier le calcul de rang car un generateur ne va pas, il faudrait le mettre dans les webelements et chaineDAction
+            ## TODO Il faut faire un dialogue entre webelements et chaineDAction
+            if action.rank == action.length:
+                onContinue = self.window.scroll()
+            ## Sinon nous ne scrollons pas et continuons la recherche en profondeur
+            else:
+                chaineDAction()
+
+
+        # print("List inter :", [i.tag_name for i in inter])
+        return list(inter) if inter else None
 
 class windowAction():
     '''
@@ -92,7 +235,7 @@ class windowAction():
         Permet un rafraîchissement du DOM de la page
         :return:
         '''
-        self.page.navigate().refresh()
+        self.page.refresh()
         self.startOfDoc()
         self.moove(self.newHeight)
 
@@ -111,13 +254,16 @@ class windowAction():
                 if self.newHeight == self.lastHeight:
                     self.startOfDoc()
                     self._endpage = True
+                    ## Nous renvoyons le False pour signifier l'arrêt de la recherche à la méthode infiniteScrollLocalize
+                    ## de la classe webelements
                     return False, self.newHeight
-                return True, self.newHeight
+                yield True
             except(Exception) as e:
                 sele_log.debug(traceback.format_exc())
                 print(e, traceback.format_exc())
                 raise
             self.lastHeight = self.newHeight
+
 
     @property
     def endpage(self):
@@ -125,12 +271,12 @@ class windowAction():
 
     def getHeight(self):
         '''Permet d'obtenir le hauteur actuelle de la fenêtre'''
-        # try:
-        return self.page.execute_script("return document.documentElement.scrollHeight;")
-        # except(Exception) as e:
-        #     sele_log.debug(traceback.format_exc())
-        #     print(e, traceback.format_exc())
-        #     raise
+        try:
+            return self.page.execute_script("return document.documentElement.scrollHeight;")
+        except(Exception) as e:
+            sele_log.debug(traceback.format_exc())
+            print(e, traceback.format_exc())
+            raise
 
     def moove(self, height=None, direction='Down'):
         '''
@@ -151,72 +297,7 @@ class windowAction():
     def startOfDoc(self):
         self.page.execute_script("window.scrollTo(0, 0)")
 
-class webElements():
-    '''
-    Serie d'éléments remontés d'une recherche, sur une fenêtre (windowAction)
-    L'objet webelement pilote l'objet windowAction qui se deplace sur la page.
-    '''
 
-    PAUSE = 4
-    DOCSTRING_LIST_WEBELEMENT = re.compile('^.*:Returns:.*- (list of WebElement).*$')
-    DOCSTRING_WEBELEMENT = re.compile('^.*:Returns:.*- (WebElement).*$')
-
-    def __init__(self, recherche, page ):
-        self.page = page
-        self.action = recherche
-        self.window = windowAction(self.page)
-
-    def seleniumRechercheBase(self, item, action, args=None):
-
-        try:
-            methodeSelenium = self.__getSeleniumMethod(item, action)
-        except(Exception) as e:
-            print(e, traceback.format_exc())
-            print("L'objet {} n'a pas de méthode {}".format(item, action))
-            raise
-        return methodeSelenium(args) if args else methodeSelenium()
-
-    def infiniteScrollLocalize(self, item, action, args=None):
-        '''
-        Methode permettant de rechercher un élément dans la page en scrollant
-        :param item: element selenium
-        :param action: Methode selenium a appliquer
-        :param args: Arguments accompagnant la methode selenium
-        :return:
-        '''
-
-        onContinue = True
-        inter = set([])
-        try:
-            itemDoc = item.__doc__.replace('\n', '')
-        except(AttributeError):
-            itemDoc = item.__doc__
-        finally:
-            if not itemDoc:
-                itemDoc = ''
-
-
-        #La boucle ici permet de rechercher des éléments à l'intérieur de la fenêtre courante
-
-        while onContinue:
-            try:
-                trouve = self.seleniumRechercheBase(item, action, args)
-                # print(trouve, self.DOCSTRING_LIST_WEBELEMENT.match(itemDoc), self.DOCSTRING_WEBELEMENT.match(itemDoc))
-                if (self.DOCSTRING_LIST_WEBELEMENT.match(itemDoc) or itemDoc is '') and trouve:
-                    for item in trouve:
-                        inter.update(item)
-                elif self.DOCSTRING_WEBELEMENT.match(itemDoc) and trouve:
-                    return trouve
-                elif item in ('click', 'drag_and_drop', 'back'):
-                    self.window.refresh()
-                    return None
-            except(Exception) as e:
-                print(e, traceback.format_exc())
-                raise
-            onContinue, newHeight = self.window.scroll()
-
-        # print("List inter :", [i.tag_name for i in inter])
-        return list(inter) if inter else None
 
 
 class webElement():
--- webcrawler/test.py
+++ webcrawler/test.py
@@ -3,22 +3,98 @@ from webcrawler.selenium_parse import *
 from pyvirtualdisplay import Display
 from selenium import webdriver
 import sys
+import pytest
 
+# @pytest.fixture
+# def display():
+#     ecran = Display(visible=BROWSER_VISIBILITY, size=RESOLUTION)
+#     ecran.start()
+#
+# @pytest.fixture()
+# def session():
+#     session = webdriver.Firefox()
+#     session.set_window_size(*RESOLUTION)
+#     session.get("https://infinite-scroll.com/demo/full-page/")
+#     return session
 
-def test_windowAction():
-    print("Début du test window action")
-    display = Display(visible=BROWSER_VISIBILITY, size=RESOLUTION)
-    display.start()
-    ####
-    session = webdriver.Firefox()
-    session.set_window_size(*RESOLUTION)
-    session.get("https://infinite-scroll.com/demo/full-page/")
-    print("La page est chargée")
-    fenetre = windowAction(session)
-    print(fenetre.getHeight())
-    fenetre.scroll()
-    print("La hauteur est de " + str(fenetre.newHeight))
-    #out, err = capsys.readouterr()
+class webelements():
+    def find_elements_by_id(self):
+        """Finds a list of elements within this element's children by ID.
+        Will return a list of webelements if found, or an empty list if not.
 
-if __name__=="__main__":
-    test_windowAction()
+        :Args:
+         - id\_ - Id of child element to find.
+
+        :Returns:
+         - list of WebElement - a list with elements if any was found.  An
+           empty list if not
+
+        :Usage:
+            elements = element.find_elements_by_id('foo')
+        """
+
+    def find_element_by_id(self):
+        """Finds a list of elements within this element's children by ID.
+        Will return a list of webelements if found, or an empty list if not.
+
+        :Args:
+         - id\_ - Id of child element to find.
+
+        :Returns:
+         - WebElement - Element if any was found.  An
+           empty list if not
+
+        :Usage:
+            elements = element.find_elements_by_id('foo')
+        """
+
+    def click(self):
+        """
+        click
+        :return:
+        """
+        return None
+
+    def glisse(self):
+
+        return None
+
+#
+# def test_windowActionScroll(display, session):
+#     print("Début du test window action scroll")
+#     display
+#     fenetre = windowAction(session)
+#     print(fenetre.getHeight())
+#     resultat = fenetre.scroll()
+#     next(resultat)
+#
+# def test_windowActionRefresh(display, session):
+#     print("Début du test Refresh")
+#     display
+#     fenetre = windowAction(session)
+#     fenetre.refresh()
+#     #out, err = capsys.readouterr()
+
+def test_returnItemTypeMulti():
+    a = webelements()
+    type = getItemType(a.find_elements_by_id)
+    assert type == "list"
+
+def test_returnItemTypeSingle():
+    a = webelements()
+    type = getItemType(a.find_element_by_id)
+    assert type == "single"
+
+def test_returnItemForClick():
+    a = webelements()
+    type = getItemType(a.click)
+    assert type == None
+
+def test_returnItemForGlisse():
+    a = webelements()
+    type = getItemType(a.glisse)
+    assert type == None
+#
+#
+# if __name__=="__main__":
+#     test_windowActionScroll(display, session)
